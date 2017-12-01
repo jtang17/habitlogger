45afd54ce5131b19fa3afadbf8caf1d8ea84616a
Brian <bbinder912@gmail.com>
2017-11-30 19:12:07 -0500

    Fix bug preventing chart from re-rendering

--- client/src/Chart.jsx
+++ client/src/Chart.jsx
@@ -5,39 +5,10 @@ import moment from 'moment';
 class Chart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      labels : this.compileEntryLabels(this.sortDates()),
-      data : this.compileEntryValues(this.sortDates()),
-      unit : this.props.unit,
-      timeframe: 0
-    };
-    this._maxDate;
-    this._minDate;
-    this._options = {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-          }
-        }],
-        xAxes: [{
-          type: 'linear',
-          ticks: {
-             suggestedMin: 0,
-             suggestedMax: this._maxDate,
-             stepSize: this._maxDate / 10 //interval between ticks
-          }
-       }]
-      }
-    };
-    this.compileEntryLabels = this.compileEntryLabels.bind(this);
+    this._maxDay;
+    this._options;
+    this.data;
     this.compileEntryValues = this.compileEntryValues.bind(this);
-    this.setData = this.setData.bind(this);
-    this.sortDates = this.sortDates.bind(this);
-  }
-
-  componentDidMount() {
-    this.setData(this.state.labels, this.state.data, this.state.unit);
   }
 
   // set entries of chart data - currently set to 15 entries
@@ -47,15 +18,6 @@ class Chart extends React.Component {
     } else {
       return entries;
     }
-  }
-
-  //Uses the sorted and filtered occurrence array that contains timestamp and value (X Axis)
-  compileEntryLabels(entries) {
-    entries = this.getLastXOccurrences(entries);
-
-    return entries.map((entry) => {
-      return this.props.timeframe + " of " + moment(entry.timestamp).format('MMM Do YYYY');
-    });
   }
 
   // getting data to be displayed on chart (Y Axis)
@@ -78,52 +40,56 @@ class Chart extends React.Component {
       }
       return { x: xDay, y: entry.value };
     });
-    return arr;
-  }
-
-  // should be unnecessary as occurrences data is sorted by database/server now - need to refactor out
-  sortDates () {
-    return this.props.occurrences.sort((a,b)=>{
-      return new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime();
-    });
-  }
-
-  // need to re-factor to get table to update/render properly
-  setData (lab,dat,uni) {
-    this.setState(  {data:
-                     {
-                       // labels: lab,
-                       datasets: [
-                         {
-                           label: uni,
-                           fill: false,
-                           lineTension: 0.1,
-                           backgroundColor: 'rgba(75,192,192,0.4)',
-                           borderColor: 'rgba(75,192,192,1)',
-                           borderCapStyle: 'butt',
-                           borderDash: [],
-                           borderDashOffset: 0.0,
-                           borderJoinStyle: 'miter',
-                           pointBorderColor: 'rgba(75,192,192,1)',
-                           pointBackgroundColor: '#fff',
-                           pointBorderWidth: 1,
-                           pointHoverRadius: 5,
-                           pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                           pointHoverBorderColor: 'rgba(220,220,220,1)',
-                           pointHoverBorderWidth: 2,
-                           pointRadius: 1,
-                           pointHitRadius: 10,
-                           data: dat,
-                         }
-                       ]
-                     }});
+    this.data = {
+      datasets: [
+        {
+          label: this.props.unit,
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: arr,
+         }
+      ]
+    };
+    this._options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+        }],
+        xAxes: [{
+          type: 'linear',
+          ticks: {
+             suggestedMin: 0,
+             suggestedMax: this._maxDate,
+             stepSize: this._maxDate / 10 //interval between ticks
+          }
+       }]
+      }
+    };
   }
 
   render() {
+    this.compileEntryValues(this.props.occurrences);
     return (
       <div id="chart">
         <h3>{this.props.habit} over the past {this._maxDay} days</h3>
-        <Line data={this.state.data} options={this._options}/>
+        <Line data={this.data} options={this._options}/>
       </div>
     );
   }
