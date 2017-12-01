ade99fb41d0d33971b00953808b89a7a19fb5f7e
Katelyn Stamas <cakelyn@gmail.com>
2017-11-30 23:01:17 -0500

    timeStamp saves whether you click on row or checkbox

--- client/src/UpdateDelete.jsx
+++ client/src/UpdateDelete.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 const UpdateDelete = () => {
   return (
+    <div>
+      <TextField type="number" min="0" hintText="Update Quantity" />
+    </div>
   )
 }
+
+export default UpdateDelete;
