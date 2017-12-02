3d2eb5997c6a0c5f324ff9badd74bee6229e8320
Katelyn Stamas <cakelyn@gmail.com>
2017-12-02 15:08:35 -0500

    social buttons for google, linkedin, twitter and email working

--- client/src/socialSharing.jsx
+++ client/src/socialSharing.jsx
@@ -5,6 +5,17 @@ import {
   generateShareIcon
 } from 'react-share';
 
+const style = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+
+  social: {
+    padding: '5px',
+  }
+}
+
 const {
   FacebookShareButton,
   GooglePlusShareButton,
@@ -21,10 +32,30 @@ const EmailIcon = generateShareIcon('email');
 
 const Social = () => {
   return (
-    <div>
-      <FacebookShareButton url={window.location.href}>
+    <div style={style.container}>
+      <FacebookShareButton style={style.social}
+                           quote="Check out my habit progress on reHabit!"
+                           url={window.location.href}>
         <FacebookIcon size={32} round={true} />
       </FacebookShareButton>
+      <TwitterShareButton style={style.social}
+                          title="Check out my habit progress on reHabit!"
+                          url={window.location.href}>
+        <TwitterIcon size={32} round={true} />
+      </TwitterShareButton>
+      <GooglePlusShareButton style={style.social} url={window.location.href}>
+        <GooglePlusIcon size={32} round={true} />
+      </GooglePlusShareButton>
+      <LinkedinShareButton style={style.social}
+                           description="Check out my habit progress on reHabit!"
+                           url={window.location.href}>
+        <LinkedinIcon size={32} round={true} />
+      </LinkedinShareButton>
+      <EmailShareButton style={style.social}
+                        subject="Check out my habit progress on reHabit!"
+                        url={window.location.href}>
+        <EmailIcon size={32} round={true} />
+      </EmailShareButton>
     </div>
   )
 }
